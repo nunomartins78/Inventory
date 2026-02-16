68cd8bec65a3420e42ff5cc9714491cd6f7556ac
Nuno Martins <nunomsmartins@gmail.com>
2026-02-16 12:38:54 +0000

    Add basic static UI scaffold (index + bootstrap)

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,9 @@
+const InventoryUI = {
+    init: function () {
+        console.log("Inventory UI loaded");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    InventoryUI.init();
+});
